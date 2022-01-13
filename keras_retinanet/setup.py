c3199c88f3ecc39b9f63303d459804ccc800328e
dinhco01 <dinhco01@users.noreply.github.com>
2022-01-13 10:27:07 +0700

    update retinanet

--- keras_retinanet/setup.py
+++ keras_retinanet/setup.py
@@ -36,8 +36,8 @@ class BuildExtension(setuptools.Command):
 
 extensions = [
     Extension(
-        'za_traffic_detection.keras_retinanet.utils.compute_overlap',
-        ['za_traffic_detection/keras_retinanet/utils/compute_overlap.pyx']
+        'keras_retinanet.utils.compute_overlap',
+        ['keras_retinanet/utils/compute_overlap.pyx']
     ),
 ]
 
